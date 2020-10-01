bf7b01e954925843723dfe2a7eff260f7ee4dcaf
shahnawazm786 <49604292+shahnawazm786@users.noreply.github.com>
2020-10-01 14:31:18 +0530

    Salary Model
    Salary Model Added

--- SalaryRespository/Salary.py
+++ SalaryRespository/Salary.py
@@ -1,2 +1,33 @@
 # -*- coding: utf-8 -*-
 
+class EmployeeSalary:
+    def __init__(self):
+        self.Empno=0
+        self.Basic=0.0
+        self.DA=0.0
+        self.TA=0.0
+        self.Phone=0.0
+        self.HRA=0.0
+        self.IT=0.0
+        
+    @property
+    def empno(self):
+        return self.Empno
+    @empno.setter
+    def empno(self,empno):
+        self.Empno=empno
+    
+    @property
+    def basic(self):
+        return self.Basic
+    @basic.setter
+    def basic(self,bs):
+        self.Basic=bs
+        
+    @property
+    def da(self):
+        return self.DA
+    @da.setter
+    def da(self,da):
+        self.DA=da
+        
